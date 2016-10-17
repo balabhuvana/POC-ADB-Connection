c3e6fcd36d73f0be57070a751593405fa032a2dd
balabhuvana <balan.ssn@gmail.com>
2016-10-17 17:14:13 +0530

    Initial commit

---
+++ app/src/main/java/admob/learn/com/poc_adb_connection/Globals.java
@@ -0,0 +1,13 @@
+package admob.learn.com.poc_adb_connection;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by becker on 10.03.15.
+ */
+public class Globals {
+    public static boolean connected = false;
+    public static Scanner socketIn;
+    public static PrintWriter socketOut;
+}
---
+++ app/src/main/java/admob/learn/com/poc_adb_connection/MainActivity.java
@@ -0,0 +1,72 @@
+package admob.learn.com.poc_adb_connection;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import java.util.Date;
+
+
+public class MainActivity extends Activity {
+
+
+    public static final String TAG = "Main";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+    }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            new Thread() {
+                @Override
+                public void run() {
+
+
+                    while (Globals.connected) {
+                        Log.d(TAG, "writing things");
+                        Globals.socketOut.write("Time: Hi bala How Are U" + System.currentTimeMillis() + "\n");
+                        Globals.socketOut.flush();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
+
+            return rootView;
+        }
+    }
+}
